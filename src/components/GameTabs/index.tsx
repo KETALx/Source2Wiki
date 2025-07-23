0d5f8110088e287cde90d96c32c7179f7333ec96
Angel <angelcazacu8@gmail.com>
2025-07-24 01:32:44 +0200

    make tabs use games constants

--- src/components/GameTabs/index.tsx
+++ src/components/GameTabs/index.tsx
@@ -2,45 +2,25 @@ import React, { ReactNode } from 'react';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import './style.css';
+import { Games } from '@site/src/constants/games';
 
-interface GameTabsProps {
-  cs2?: ReactNode;
-  steamvr?: ReactNode;
-  dota2?: ReactNode;
-  hla?: ReactNode;
-}
-
-const GameTabs: React.FC<GameTabsProps> = ({
-  cs2,
-  steamvr,
-  dota2,
-  hla
-}) => {
+const GameTabs: React.FC<Record<string, React.ReactNode>> = ({ gameContent }) => {
   return (
     <Tabs queryString="game" className="game-tabs">
-      {cs2 && (
-        <TabItem value="cs2" label="Counter-Strike 2" attributes={{className: "cs2"}}>
-          {cs2}
-        </TabItem>
-      )}
-     
-      {steamvr && (
-        <TabItem value="steamvr" label="SteamVR Home" attributes={{className: "steamvr"}}>
-          {steamvr}
-        </TabItem>
-      )}
-     
-      {dota2 && (
-        <TabItem value="dota2" label="Dota 2" attributes={{className: "dota2"}}>
-          {dota2}
-        </TabItem>
-      )}
-     
-      {hla && (
-        <TabItem value="hla" label="Half-Life: Alyx" attributes={{className: "hla"}}>
-          {hla}
-        </TabItem>
-      )}
+      {Object.entries(gameContent).map(([gameKey, content]) => {
+        const gameInfo = Games[gameKey];
+        if (!gameInfo || !content) return null;
+        return (
+          <TabItem 
+            key={gameKey}
+            value={gameKey} 
+            label={gameInfo.PrettyName} 
+            attributes={{className: gameKey}}
+          >
+            {content}
+          </TabItem>
+        );
+      })}
     </Tabs>
   );
 };
